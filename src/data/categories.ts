b8b10f9830ed1d33b6989814ab9a5bf879b6b9b2
Rachit Tachamo <tachamorachit7@gmail.com>
2025-05-14 01:17:36 +0545

    changes on images and added some contents

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -4,27 +4,31 @@ export const categories = [
     name: "Trekking & Adventure",
     description: "Explore Nepal's most breathtaking hiking trails and outdoor experiences",
     slug: "trekking-adventure",
-    image: "/images/trekking.jpeg"
+    image: "/images/trekking.jpeg",
+    textColor: "text-black" 
   },
   {
     id: "2", 
     name: "Cultural Experiences",
     description: "Discover the rich traditions, festivals, and heritage of Nepal",
     slug: "cultural-experiences",
-    image: "/images/bisket-jatra-bhaktapur.jpg"
+    image: "/images/bisket-jatra-bhaktapur.jpg",
+    textColor: "text-black"
   },
   {
     id: "3",
     name: "Local Cuisine",
     description: "Savor the flavors of Nepalese food and culinary traditions",
     slug: "local-cuisine",
-    image: "/images/newarikhaja.jpeg"
+    image: "/images/newarikhaja.jpeg",
+    textColor: "text-black"
   },
   {
     id: "4",
     name: "Wildlife & Nature",
     description: "Experience Nepal's diverse ecosystems and magnificent wildlife",
     slug: "wildlife-nature",
-    image: "/images/rhino.jpeg"
+    image: "/images/rhino.jpeg",
+    textColor: "text-black"
   }
 ];
